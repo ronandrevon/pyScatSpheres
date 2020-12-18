787d2eb51bfd2fcfb5423f9a91a6e32f5710e810
tdrevon <ronandrevon@gmail.com>
2020-12-18 10:17:48 +0000

    added data to package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyScatSpheres",
-    version="1.0.3.dev2",
+    version="1.0.3.dev3",
     author="Tarik Ronan Drevon",
     author_email="tarik.drevon@stfc.ac.uk",
     description="Scattering of array of spheres, scalar theory",
@@ -16,12 +16,15 @@ setuptools.setup(
         'Documentation': 'https://pyscatspheres.readthedocs.io/en/latest/',
         'Source':'https://github.com/ronandrevon/pyScatSpheres',
     },
-    packages=setuptools.find_packages(),
+    keywords=["diffraction","spherical","scalar","wave","array"],
+    python_requires='>=3.6',
+    packages=['pyScatSpheres'],
+    include_package_data=True,
+    package_data={'':['data*.pkl']},
+    install_requires=['numpy','scipy','pandas','colorama','py3nj','matplotlib','easygui'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License ",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
-    install_requires=['numpy','scipy','pandas','colorama','py3nj','matplotlib','easygui'],
 )
