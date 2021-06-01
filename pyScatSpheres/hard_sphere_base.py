fixed bugs for displaying radial derivative for N>1. Improvments still required

pyScatSpheres/hard_sphere_base.py
@@ -40,7 +40,7 @@ class HardSphereArrayBase():
         k,N,nmax = self.k,self.N,self.nmax
         ap,dp = self.ka/k,self.kd/k
 
-        if idp:self._check_idp(idp=None)
+        self._check_idp(idp);
         args = {}
         if def_args:
             args = {'lw':2,'labs':['$y$','$z$'],'imOpt':'c','axPos':'V','fonts':{'title':25},}
@@ -54,24 +54,24 @@ class HardSphereArrayBase():
         Gopt =  ''.join([c for c in opts if c in 'GF'] )
         name+=['f','df']['G' in opts]
         if 'P' in opts:
-            for p in idp:
-                if v:print("...compute field at sphere p=%d..." %p)
-                fi,fs = self.compute_f(r,theta,0,ftype='a',Gopt=Gopt,idp=p)
-                if 'i' in opts:
-                    f = fz(fi)
-                    dsp.stddisp(plts,im=[y,z,f],
-                        title = r"Incident %s at sphere %d, $N_{max}=%d$" %(fstr,p,nmax),
-                        name=name+'i_sphere%d.png' %p,**args,**kwargs)
-                if 's' in opts:
-                    f = fz(fs)
-                    dsp.stddisp(plts,im=[y,z,f],
-                        title = r"Scattered %s at sphere %d, $N_{max}=%d$" %(fstr,p,nmax),
-                        name=name+'s_sphere%d.png' %p,**args,**kwargs)
-                if 't' in opts:
-                    f = fz(fi+fs)
-                    dsp.stddisp(plts,im=[y,z,f],
-                        title = r"Total %s at sphere %d, $N_{max}=%d$" %(fstr,p,nmax),
-                        name=name+'t_sphere%d.png' %p,**args,**kwargs)
+            p=idp
+            if v:print("...compute field at sphere p=%d..." %p)
+            fi,fs = self.compute_f(r,theta,0,ftype='a',Gopt=Gopt,idp=p)
+            if 'i' in opts:
+                f = fz(fi)
+                dsp.stddisp(plts,im=[y,z,f],
+                    title = r"Incident %s at sphere %d, $N_{max}=%d$" %(fstr,p,nmax),
+                    name=name+'i_sphere%d.png' %p,**args,**kwargs)
+            if 's' in opts:
+                f = fz(fs)
+                dsp.stddisp(plts,im=[y,z,f],
+                    title = r"Scattered %s at sphere %d, $N_{max}=%d$" %(fstr,p,nmax),
+                    name=name+'s_sphere%d.png' %p,**args,**kwargs)
+            if 't' in opts:
+                f = fz(fi+fs)
+                dsp.stddisp(plts,im=[y,z,f],
+                    title = r"Total %s at sphere %d, $N_{max}=%d$" %(fstr,p,nmax),
+                    name=name+'t_sphere%d.png' %p,**args,**kwargs)
         if 'T' in opts:
             if v:print("...compute field from all spheres ...")
             fi,fs = self.compute_f(r,theta,0,ftype='a',Gopt=Gopt)
@@ -132,14 +132,8 @@ class HardSphereArrayBase():
     ############################################################################
     def _check_idp(self,idp=None):
         if isinstance(idp,int) :
-            if idp>=0 and idp<self.N:
-                idp=[idp]
-            else:
-                print(colors.red+'warn:sphere index not within 0<=idp<N'+colors.black)
-                idp=np.arange(self.N)
-        else:
-            idp=np.arange(self.N)
-        return idp
+            if not (idp>=0 and idp<self.N):
+                raise Exception(colors.red+'sphere index not within 0<=idp<N'+colors.black)
 
     def test_convergence(self,nmaxs,npts=361,name='', **kwargs):
         ns = np.array(nmaxs).size

pyScatSpheres/qdot_sphere_array.py
@@ -88,19 +88,21 @@ class QdotSphereArray(hsb.HardSphereArrayBase):
         '''
         k,n_p,d_p,nmax,N = self.k,self.kp,self.d_p,self.nmax,self.N
         x,y,z = spu.sphere2cart(r,theta,phi)
-        idp = self._check_idp(idp) #;print(idp)
+        self._check_idp(idp) #;print(idp)
 
         #incident wave
         if ftype in 'ita':
             fi = np.zeros(r.shape,dtype=complex)
             gi = np.zeros(r.shape,dtype=complex)
             # plane wave at sphere idp : use local spherical decomposition
-            if isinstance(idp,list):
+            if isinstance(idp,int):
                 # print('incident field at p sphere')
-                r_p,theta_p,phi_p = spu.cart2sphere(x,y,z-self.d_p[idp[0]])
+                r_p,theta_p,phi_p = spu.cart2sphere(x,y,z-self.d_p[idp])
                 for l in range(nmax):
                     fi += 1J**l*(2*l+1)*spu.jn(l,k*r_p)*spu.Pl(l,theta_p)
+                    gi += 1J**l*(2*l+1)*spu.jnp(l,k*r_p)*spu.Pl(l,theta_p)*k
                 fi *= np.exp(1J*k*self.d_p[idp])
+                gi *= np.exp(1J*k*self.d_p[idp])
             # Otherwise actual plane wave propagating along z
             else:
                 # print('full incident field')
@@ -114,17 +116,19 @@ class QdotSphereArray(hsb.HardSphereArrayBase):
                 gi[idx_i] = 0
 
         #scattered fields
+        # idp = range(N)
         if ftype in 'sta':
             fs = np.zeros(r.shape,dtype=complex)
             gs = np.zeros(r.shape,dtype=complex)
             #outgoing field
-            for p in idp:
+            for p in range(N):
                 r_p,theta_p,phi_p = spu.cart2sphere(x,y,z-self.d_p[p])
                 idx_o = r_p>=self.ka     #;print(idx_o.shape)
                 for l in range(nmax):
                     Yl = spe.sph_harm(0,l,0,theta_p)
                     fs[idx_o] += self.bp[p*nmax+l]*spu.hn1(l, k*r_p[idx_o])*Yl[idx_o]
                     gs[idx_o] += self.bp[p*nmax+l]*spu.hn1p(l,k*r_p[idx_o])*Yl[idx_o]
+                    #gs SHOULD USE THE TRANSLATION TO GET RADIAL DERIVATIVE r_p
             #remove scattered field inside spheres
             for p in range(N):
                 r_p,theta_p,phi_p = spu.cart2sphere(x,y,z-self.d_p[p])
@@ -132,7 +136,7 @@ class QdotSphereArray(hsb.HardSphereArrayBase):
                 fs[idx_i] = 0
                 gs[idx_i] = 0
             #inside field
-            for p in idp:
+            for p in range(N):
                 r_p,theta_p,phi_p = spu.cart2sphere(x,y,z-self.d_p[p])
                 idx_i = r_p<self.ka     #;print(idx_i.shape)
                 for l in range(nmax):

tests/plot_gaunt_coeffs.py
@@ -44,15 +44,15 @@ def plot_psi_lm(l,m,zl='in',zq='',bbt={}, phi=0,cart_opt=True,npts=100,r=(1e-3,5
 ###########################################################################################################
 #                                               Tests
 ###########################################################################################################
-opts= '' #i(in-in), o(out-out)g(gaunt)
+opts= 'o' #i(in-in), o(out-out)g(gaunt)
 name,opt=path+'psi','p'
 
 Nmax=1
 ls = np.hstack([[n]*(n+1) for n in np.arange(Nmax)])
 ms = np.hstack([np.arange(n+1) for n in np.arange(Nmax)])
+phi,npts,R = 0, 100, 2
 
 if 'i' in opts:
-    phi,npts,R = 0, 100, 2
     xylims,xylims0 = 2*R*np.array([1e-3,1,0,1]),R*np.array([-1,1,-1,1])
     bbt = {'r_d':6,'theta_d':2*np.pi/3,'phi_d':np.pi, 'Nmax':10}
     for l,m in zip(ls,ms): plot_psi_lm(l=l,m=m,zl='in',zq=''  ,bbt={} ,phi=phi,cart_opt=True,npts=npts,r=xylims0,