9b01450549d30813081b6306b1c379912da75338
Kitine <christine.quach.2022@ensta-paris.fr>
2021-07-28 00:23:01 +0200

    cas linéaire marche presque, pb avec fct th de translation

--- tests/Spherical_Quantum_Dot_Array_arb.py
+++ tests/Spherical_Quantum_Dot_Array_arb.py
@@ -9,7 +9,12 @@ from pyScatSpheres import harmo_sphe as hs
 import time
 import pickle
 
-qdot1 = qsa_arb.QdotSphereArray(ka=1,kd_z=np.array([2,4]),kd_y=np.array([0,0]),kp=1.1,nmax=3)
-
-print(qdot1.ap)
+qdot1 = qsa_arb.QdotSphereArray(ka=1,kd_z=np.array([1,3,5]),kd_y=np.array([0,0,0]),kp=1.1,nmax=1)
+qdot2=qsa.QdotSphereArray(ka=1,kd=np.array([1,3,5]),kp=1.1,nmax=1,copt=1,N=3)
+#print(qdot1.ap)
+#print(qdot2.ap)
+#print("\n")
 print(qdot1.bp)
+print(qdot2.bp)
+
+#pour nmax ca marche, mais quand j'augmente le nb d'atomes ca marche plus... grrrr
